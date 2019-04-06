3860c5c386252870ed519dc968bf69dd84f3a4a8
MonikaZiaja <41911868+MonikaZiaja@users.noreply.github.com>
2019-04-06 13:30:34 +0200

    initial commit

---
+++ precache-manifest.3736af9141ce53fcd96a48076e2f3118.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cf34c1d2d7f54d4e9f8725f44755ee4",
+    "url": "/KNZMPage/static/media/headerImg.1cf34c1d.jpg"
+  },
+  {
+    "revision": "336ad600d36fe25465e3",
+    "url": "/KNZMPage/static/css/main.8c792d0b.chunk.css"
+  },
+  {
+    "revision": "217cbd6679eb01e5a47e",
+    "url": "/KNZMPage/static/js/1.217cbd66.chunk.js"
+  },
+  {
+    "revision": "98013005316d7d741c76",
+    "url": "/KNZMPage/static/js/runtime~main.98013005.js"
+  },
+  {
+    "revision": "f3aeb3bda2b74b66f752cd38ae9ae5ef",
+    "url": "/KNZMPage/static/media/partnerImg.f3aeb3bd.jpg"
+  },
+  {
+    "revision": "d8d9d14ff203329c9920a89a2c188a24",
+    "url": "/KNZMPage/static/media/people.d8d9d14f.jpg"
+  },
+  {
+    "revision": "945b6ad3307387f5cc35b01459256377",
+    "url": "/KNZMPage/static/media/karty_AW.945b6ad3.jpg"
+  },
+  {
+    "revision": "1891225e00d58db83b5b09ef7d3c7e1b",
+    "url": "/KNZMPage/static/media/aboutUsImg.1891225e.jpg"
+  },
+  {
+    "revision": "77ecf82aa15c2147b2e25e9da9c5c269",
+    "url": "/KNZMPage/static/media/headerImg1.77ecf82a.jpg"
+  },
+  {
+    "revision": "336ad600d36fe25465e3",
+    "url": "/KNZMPage/static/js/main.336ad600.chunk.js"
+  },
+  {
+    "revision": "c6c9a9d888471afdf994f1176f566a9c",
+    "url": "/KNZMPage/static/media/conferenceImg.c6c9a9d8.jpg"
+  },
+  {
+    "revision": "400b379dd74a90a60dc25574a5832450",
+    "url": "/KNZMPage/static/media/contact.400b379d.jpg"
+  },
+  {
+    "revision": "6dd9d1063d6c3458e7d89e7050c98f4b",
+    "url": "/KNZMPage/static/media/conferenceImg2.6dd9d106.jpg"
+  },
+  {
+    "revision": "2fd2a756336b25152fb7170f2d96863d",
+    "url": "/KNZMPage/static/media/headerImg1Dark.2fd2a756.jpg"
+  },
+  {
+    "revision": "a4272d18f2041e2e53a7a62937de9c68",
+    "url": "/KNZMPage/static/media/admin3.a4272d18.jpg"
+  },
+  {
+    "revision": "a4272d18f2041e2e53a7a62937de9c68",
+    "url": "/KNZMPage/static/media/admin.a4272d18.jpg"
+  },
+  {
+    "revision": "a4272d18f2041e2e53a7a62937de9c68",
+    "url": "/KNZMPage/static/media/admin2.a4272d18.jpg"
+  },
+  {
+    "revision": "a4272d18f2041e2e53a7a62937de9c68",
+    "url": "/KNZMPage/static/media/admin4.a4272d18.jpg"
+  },
+  {
+    "revision": "8d72dd604ba8151ba44666669cc24e5f",
+    "url": "/KNZMPage/index.html"
+  }
+];
